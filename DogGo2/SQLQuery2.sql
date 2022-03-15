81ee9c5e89c6ecd6739074b44c9c248651d9f595
gabriel smith <damngabe1@gmail.com>
2022-03-15 13:14:11 -0500

    Forgot Owner Detail View

--- DogGo2/SQLQuery2.sql
+++ DogGo2/SQLQuery2.sql
@@ -1,2 +1,14 @@
 ﻿SELECT *
 FROM Owner
+
+SELECT w.NeighborhoodId
+FROM Walker w
+LEFT JOIN Neighborhood N on w.NeighborhoodId = N.Id
+
+SELECT w.Id, w.[Name], w.ImageUrl, w.NeighborhoodId, n.[Name] AS Neighborhood
+FROM Walker w
+LEFT JOIN Neighborhood n on w.NeighborhoodId = n.id
+
+SELECT *
+FROM Walker, Neighborhood
+WHERE Walker.NeighborhoodId = Neighborhood.Id
